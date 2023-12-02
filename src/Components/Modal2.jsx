98023003dc6adca3f4df7153aaf005770d9d7250
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-12-02 01:30:02 -0500

    WIP

--- src/Components/Modal2.jsx
+++ src/Components/Modal2.jsx
@@ -36,22 +36,6 @@ export const Modal2 = ({ openModal2, setOpenModal2 }) => {
           <div className="modal-container">
 
           <div className="modalContent">
-          {/* {tokenList?.map((e, i) => {
-            return (
-              <div
-                className={styles.tokenChoice}
-                key={i}
-                onClick={() => modifyToken(i)}
-              >
-                <img src={e.img} alt={e.ticker} className={styles.tokenLogo} />
-                <div className={styles.tokenChoiceNames}>
-                  <div className={styles.tokenName}>{e.name}</div>
-                  <div className={styles.tokenTicker}>{e.ticker}</div>
-                </div>
-              </div>
-            );
-          })} */}
-
 
           </div>
             <h3 className="">Token List</h3>
--- src/Components/TokenList.jsx
+++ src/Components/TokenList.jsx
@@ -3,98 +3,17 @@ import tokenList from "../resources/data/tokenEthList.json";
 import styles from '../styles/App.module.css'
 
 
-function modifyToken(idx) {
-    alert("Modifying Token " + idx);
+function modifyToken(obj) {
+    alert("Modifying Token Object" + JSON.stringify(obj,null,2));
   }
-
-  function getTokenListBroke(){
-    <div>
-        {tokenList?.map((e, i) => {
-            return (
-            <div
-                className={styles.tokenChoice}
-                key={i}
-                onClick={() => modifyToken(i)}
-            >
-                <img src={e.img} alt={e.ticker} className={styles.tokenLogo} />
-                <div className={styles.tokenChoiceNames}>
-                    {alert("e.name[" +i+ "] = " + e.name)}
-                    <div className={styles.tokenName}>{e.name}</div>
-                    <div className={styles.tokenTicker}>{e.ticker}</div>
-                </div>
-            </div>
-            );
-        })}
-    </div>
-}
-
-function getTokenList(){
-    <div>
-        return (
-            <div>
-            tokenList[1].name
-            tokenList[1].ticker
-            </div>
-        )
-
-        {/* {tokenList?.map((e, i) => {
-            return (
-            <div
-                className={styles.tokenChoice}
-                key={i}
-                onClick={() => modifyToken(i)}
-            >
-                <img src={e.img} alt={e.ticker} className={styles.tokenLogo} />
-                <div className={styles.tokenChoiceNames}>
-                    {alert("e.name[" +i+ "] = " + e.name)}
-                    <div className={styles.tokenName}>{e.name}</div>
-                    <div className={styles.tokenTicker}>{e.ticker}</div>
-                </div>
-            </div>
-            );
-        })} */}
-    </div>
-}
-
-function TokenList1() {
-    //    {alert(JSON.stringify(tokenList, null, 2))}
-    return (
-        <div>
-            <h2>{tokenList[1].name} {tokenList[1].ticker}</h2>
-        </div>
-    )
-    // })}
-
-}
-
-
-
-function TokenList2() {
-  tokenList?.map((e, i) => {
-        return (
-            <div
-                className={styles.tokenChoice}
-                key={i}
-                onClick={() => modifyToken(i)}
-            >
-                <img src={e.img} alt={e.ticker} className={styles.tokenLogo} />
-                <div className={styles.tokenChoiceNames}>
-                    {/* {alert("e.name[" +i+ "] = " + e.name)} */}
-                    <div className={styles.tokenName}>{e.name}</div>
-                    <div className={styles.tokenTicker}>{e.ticker}</div>
-                </div>
-            </div>
-            );
-        })}
 
     function TokenList() {
         // {alert(JSON.stringify(tokenList, null, 2))}
         const tList = tokenList?.map((e, i) => (
-            
+
             <div
                 className={styles.tokenChoice}
-                key={i}
-                onClick={() => modifyToken(i)}
+                onClick={() => modifyToken(tokenList[i])}
             >
                 <img src={e.img} alt={e.ticker} className={styles.tokenLogo} />
 
@@ -104,15 +23,13 @@ function TokenList2() {
                     <div className={styles.tokenTicker}>{e.ticker}</div> 
                 </div>
             </div>)
-
         )
         
-            return (
-                <div>
-                    {tList}
-                </div>
-            )
-        }
+        return (
+            <div>
+                {tList}
+            </div>
+        )
+    }
 
 export default TokenList
-
